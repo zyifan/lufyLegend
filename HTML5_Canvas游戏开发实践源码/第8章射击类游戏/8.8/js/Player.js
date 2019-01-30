ce043db79274bbcd8c9a8e038705b3d4465093da
zyifan <504069264@qq.com>
2019-01-30 17:57:27 +0800

    添加注释

--- HTML5_Canvas游戏开发实践源码/第8章射击类游戏/8.8/js/Player.js
+++ HTML5_Canvas游戏开发实践源码/第8章射击类游戏/8.8/js/Player.js
@@ -4,7 +4,9 @@
 function Player(x,y,shootX,shootY,bitmapData,hp){
 	base(this,Plain,[x,y,shootX,shootY,bitmapData,hp]);
 	var self = this;
+	// 为了区分敌机，我机，设置我机的belong属性为self
 	self.belong = "self";
+	// 记录鼠标按下时发觉的位置
 	self.downX = self.downY = 0;
 	self.bulletBitmapData=new LBitmapData(imglist["bullet01"]);
 }
